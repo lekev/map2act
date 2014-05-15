552ead1d5b936cfd2312cd543644ac9f10aec3ae
Renous <kevinrenous@gmail.com>
2014-05-15 11:28:06 +0300

    change 15 mai

--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
-TYPES = [["יוזמה חברתית", 'social'],
-          ['עסק חברתי', 'coworking'],
+TYPES = [['עסק חברתי', 'coworking'],
+		["יוזמה חברתית", 'social'],
           ['קרנות ומשקיעים', 'investor'],
           ['חממות ומאיצים', 'accelerator'],
           ['אקדמיה והכשרה', 'academy']]
