3d731c36457416d382682af6b501f1dfb30b9232
Renous <kevinrenous@gmail.com>
2014-09-15 13:46:06 +0300

    form south change javascript

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -19,27 +19,27 @@ $("#place_type").change ->
 */
 
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
 
-    $(".label_description_big").text("תארו באיזה אופן אתם מעוניינים לאפשר לצרכנים לצרוך מכם : ציינו ימי מכירה מרוכזים - מיקום ושעות, כתובת להזמנות אונליין, מבצעים, הנחות, וכל מידע שנראה לכם רלוונטי.")
-    $('#place_type').on('change', function() {
-        lab_descr = $(".label_description_big");
-        if (this.value == 'south_company') {
+//     $(".label_description_big").text("תארו באיזה אופן אתם מעוניינים לאפשר לצרכנים לצרוך מכם : ציינו ימי מכירה מרוכזים - מיקום ושעות, כתובת להזמנות אונליין, מבצעים, הנחות, וכל מידע שנראה לכם רלוונטי.")
+//     $('#place_type').on('change', function() {
+//         lab_descr = $(".label_description_big");
+//         if (this.value == 'south_company') {
 
-            lab_descr.text("תארו באיזה אופן אתם מעוניינים לאפשר לצרכנים לצרוך מכם ציינו ימי מכירה מרוכזים - מיקום ושעות, כתובת להזמנות אונליין, מבצעים, הנחות, וכל מידע שנראה לכם רלוונטי .");
-        } else {
-            lab_descr.text("איזה שינוי חברתי אתם שואפים ליצור וכיצד אתם מבצעים זאת?");
-        }
-    });
+//             lab_descr.text("תארו באיזה אופן אתם מעוניינים לאפשר לצרכנים לצרוך מכם ציינו ימי מכירה מרוכזים - מיקום ושעות, כתובת להזמנות אונליין, מבצעים, הנחות, וכל מידע שנראה לכם רלוונטי .");
+//         } else {
+//             lab_descr.text("איזה שינוי חברתי אתם שואפים ליצור וכיצד אתם מבצעים זאת?");
+//         }
+//     });
 
-    $('#modal_add').on('shown', function() {
+//     $('#modal_add').on('shown', function() {
 
-        var latlng = new google.maps.LatLng(31.9298264, 34.768551);
+//         var latlng = new google.maps.LatLng(31.9298264, 34.768551);
 
-        google.maps.event.trigger(map_form, "resize");
-        map_form.setCenter(latlng);
+//         google.maps.event.trigger(map_form, "resize");
+//         map_form.setCenter(latlng);
 
-    });
+//     });
 
-});
+// });
